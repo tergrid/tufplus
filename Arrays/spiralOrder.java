9882e0b9435871e3b8d8651c0d2a2a3d91b9c565
tergrid <harshitsinghkalsi@gmail.com>
2025-02-07 14:55:12 +0530

    commit

--- Arrays/spiralOrder.java
+++ Arrays/spiralOrder.java
@@ -42,8 +42,6 @@ class spiralOrder {
     int[][] mat = {
         { 1, 2, 3, 4 },
         { 5, 6, 7, 8 },
-        { 9, 10, 11, 12 },
-        { 13, 14, 15, 16 }
     };
     spiralOrder so = new spiralOrder();
     ArrayList<Integer> ansList = so.spiralOrderFn(mat);
